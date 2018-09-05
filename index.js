6fdeffce7a9fa75238373a20da4c9992b53d2366
Jacob Menashi <jbmenashi@gmail.com>
2018-09-05 21:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
+<<<<<<< HEAD
+=======
+`Hello ${specialGuest}!`
+>>>>>>> 7884b3db7195216fcc970d6e90833a0b3d1d0633
 
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
